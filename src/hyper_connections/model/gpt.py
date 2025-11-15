- allow pos embed - fix swiglu 2/3 scaling - simplify pre-/post-norm logic - use GPTConfig dataclass - [untested] optional flash attention - allow shuffle in tokenized dataset

src/hyper_connections/model/gpt.py
@@ -1,20 +1,37 @@
 import torch
 import huggingface_hub as hf
+from dataclasses import dataclass
 
-from model.transformer import Transformer
+from hyper_connections.model.transformer import Transformer
+
+
+@dataclass(frozen=True)
+class GPTConfig:
+    vocab_size: int
+    dim: int
+    num_heads: int
+    num_layers: int
+    base: int | None = 10_000
+    n_ctx: int | None = None
+    padding_idx: int | None = None
 
 
 class GPT(torch.nn.Module):
-    def __init__(self, vocab_size, dim, num_heads, num_layers, base, padding_idx=None):
+    def __init__(self, config: GPTConfig):
         super().__init__()
-        self.token_emb = torch.nn.Embedding(vocab_size, dim, padding_idx=padding_idx)
-        self.norm_gen = lambda: torch.nn.LayerNorm(dim, bias=False)
-        self.transformer = Transformer(dim, num_heads, num_layers, base, self.norm_gen)
-        self.ln_f = torch.nn.LayerNorm(dim, bias=False)
-        self.head = torch.nn.Linear(dim, vocab_size, bias=False)
+        self.token_emb = torch.nn.Embedding(config.vocab_size, config.dim, padding_idx=config.padding_idx)
+        if config.base is None:
+            assert isinstance(config.n_ctx, int), "n_ctx must be provided if base is None"
+            self.pos_emb = torch.nn.Embedding(config.n_ctx, config.dim)
+        norm_gen = lambda: torch.nn.LayerNorm(config.dim, bias=False)
+        self.transformer = Transformer(config.dim, config.num_heads, config.num_layers, norm_gen, pre_norm=True, R=config.base)
+        self.ln_f = torch.nn.LayerNorm(config.dim, bias=False)
+        self.head = torch.nn.Linear(config.dim, config.vocab_size, bias=False)
 
     def forward(self, x, y=None):
         x = self.token_emb(x)
+        if hasattr(self, "pos_emb"):
+            x = x + self.pos_emb.weight[None, : x.size(1), :]
         x = self.transformer(x)
         x = self.ln_f(x)
 

src/hyper_connections/model/hc.py
@@ -3,8 +3,9 @@ import huggingface_hub as hf
 import torch
 from typing import Callable
 
-from hyper_connections.model.act import SwiGLU
-from hyper_connections.model.transformer import MLP, RotaryAttention
+from hyper_connections.model.act import SwiGLU, ReLU
+from hyper_connections.model.transformer import MLP, Attention
+from hyper_connections.model.gpt import GPTConfig
 
 
 class HCNet(torch.nn.Module):
@@ -55,25 +56,30 @@ class HCNet(torch.nn.Module):
 
 
 class HCTransformer(HCNet):
-    def __init__(self, dim, num_heads, num_layers, base, expansion_rate=2):
+    def __init__(self, D, H, L, norm_gen, pre_norm: bool = True, R: int | None = None, expansion_rate: int = 2):
         blocks = torch.nn.ModuleList()
-        for _ in range(num_layers):
-            blocks.append(RotaryAttention(base, dim, num_heads))
-            # blocks.append(MLP([dim, dim // 3 * 2, dim], SwiGLU()))
-            blocks.append(MLP([dim, dim * 4, dim], SwiGLU()))
-        norm_gen = lambda: torch.nn.LayerNorm(dim, bias=False)
-        super().__init__(blocks, norm_gen=norm_gen, expansion_rate=expansion_rate)
+        for _ in range(L):
+            blocks.append(Attention(D, H, R=R))
+            blocks.append(MLP([D, D // 3 * 8, D], SwiGLU()))
+            # blocks.append(MLP([D, D * 4, D], ReLU()))
+        super().__init__(blocks, norm_gen=norm_gen, pre_norm=pre_norm, expansion_rate=expansion_rate)
 
 
 class HCGPT(torch.nn.Module):
-    def __init__(self, vocab_size, dim, num_heads, num_layers, base, expansion_rate=2, padding_idx=None):
+    def __init__(self, config: GPTConfig, expansion_rate=2):
         super().__init__()
-        self.token_emb = torch.nn.Embedding(vocab_size, dim, padding_idx=padding_idx)
+        self.token_emb = torch.nn.Embedding(config.vocab_size, config.dim, padding_idx=config.padding_idx)
         self.transformer = HCTransformer(
-            dim, num_heads, num_layers, base, expansion_rate=expansion_rate
+            config.dim,
+            config.num_heads,
+            config.num_layers,
+            norm_gen=lambda: torch.nn.LayerNorm(config.dim, bias=False),
+            pre_norm=True,
+            R=config.base,
+            expansion_rate=expansion_rate,
         )
-        self.ln_f = torch.nn.LayerNorm(dim, bias=False)
-        self.head = torch.nn.Linear(dim, vocab_size, bias=False)
+        self.ln_f = torch.nn.LayerNorm(config.dim, bias=False)
+        self.head = torch.nn.Linear(config.dim, config.vocab_size, bias=False)
 
     def forward(self, x, y=None):
         x = self.token_emb(x)

src/hyper_connections/model/transformer.py
@@ -12,29 +12,13 @@ import torch
 import einops
 from typing import Callable
 
-from hyper_connections.model.act import Activation, SwiGLU
+from hyper_connections.model.act import Activation, SwiGLU, ReLU
 from hyper_connections.model.rope import Rotary, apply_rotary_pos_emb
 
 
 class ResNet(torch.nn.Module):
     """
     A residual network is a sequence of blocks with skip connections.
-
-    Notation:
-        - X: input, shape (B, N, D)
-        - T_i: i-th block, maps (N, D) -> (N, D)
-
-    Forward pass:
-        for each block T_i:
-            H = X
-            if pre-norm:
-                H = norm(H)
-            H = T_i(H)
-
-            X = X + H
-            if not pre-norm:
-                X = norm(X)
-        return X
     """
 
     def __init__(
@@ -47,13 +31,10 @@ class ResNet(torch.nn.Module):
 
     def forward(self, x_BND: torch.Tensor) -> torch.Tensor:
         for block, norm in zip(self.blocks, self.norms):
-            h_BND = x_BND
             if self.pre_norm:
-                h_BND = norm(h_BND)
-            h_BND = block(h_BND)
-            x_BND = x_BND + h_BND
-            if not self.pre_norm:
-                x_BND = norm(x_BND)
+                x_BND = x_BND + block(norm(x_BND))
+            else:
+                x_BND = norm(x_BND + block(x_BND))
         return x_BND
 
 
@@ -63,52 +44,56 @@ class Transformer(ResNet):
         D: int,
         H: int,
         L: int,
-        R: int,
-        norm: torch.nn.Module,
+        norm_gen: Callable[[], torch.nn.Module],
         pre_norm: bool = True,
+        R: int | None = None,
     ):
         blocks = torch.nn.ModuleList()
         for _ in range(L):
-            blocks.append(RotaryAttention(R, D, H))
-            # blocks.append(MLP([D, D // 3 * 4, D], SwiGLU()))
-            blocks.append(MLP([D, D * 4, D], SwiGLU()))
-        super().__init__(blocks, norm, pre_norm)
+            blocks.append(Attention(D, H, R))
+            blocks.append(MLP([D, D // 3 * 8, D], SwiGLU()))
+            # blocks.append(MLP([D, D * 4, D], ReLU()))
+        super().__init__(blocks, norm_gen, pre_norm)
 
 
-class RotaryAttention(torch.nn.Module):
-    def __init__(self, R: int, D: int, H: int):
+class Attention(torch.nn.Module):
+    def __init__(self, D: int, H: int, R: int | None = None):
         super().__init__()
         self.H = H
         self.D = D
         self.K = D // H
 
-        self.rotary = Rotary(self.K, base=R)
+        self.rotary = None if R is None else Rotary(self.K, base=R)
         self.qkv = torch.nn.Linear(D, 3 * self.H * self.K, bias=False)
         self.out = torch.nn.Linear(self.D, D, bias=False)
 
-    def forward(self, x_BND: torch.Tensor, causal: bool = True):
+    def forward(self, x_BND: torch.Tensor, causal: bool = True, flash: bool = False) -> torch.Tensor:
         B, N, D = x_BND.shape
 
         qkv_BNHK = self.qkv(x_BND).view(B, N, 3, self.H, self.K)
         q_BNHK, k_BNHK, v_BNHK = qkv_BNHK.unbind(dim=2)
 
-        cos, sin = self.rotary(q_BNHK, seq_dim=1)
-        q_BNHK, k_BNHK = apply_rotary_pos_emb(q_BNHK, k_BNHK, cos, sin)
+        if self.rotary is not None:
+            cos, sin = self.rotary(q_BNHK, seq_dim=1)
+            q_BNHK, k_BNHK = apply_rotary_pos_emb(q_BNHK, k_BNHK, cos, sin)
 
-        attn_scores_BNHH = einops.einsum(
-            q_BNHK, k_BNHK, "B N H K, B M H K -> B N H M"
-        ) / (self.K**0.5)
-
-        if causal:
-            mask_NM = torch.tril(torch.ones(N, N, device=x_BND.device)).bool()
-            attn_scores_BNHH = attn_scores_BNHH.masked_fill(
-                ~mask_NM[None, :, None, :], float("-inf")
+        if flash and torch.cuda.is_available() and torch.__version__ >= "2.0.0":
+            attn_output_BNHK = torch.nn.functional.scaled_dot_product_attention(
+                q_BNHK, k_BNHK, v_BNHK, is_causal=causal
             )
-
-        attn_probs_BNHH = torch.nn.functional.softmax(attn_scores_BNHH, dim=-1)
-        attn_output_BNHK = einops.einsum(
-            attn_probs_BNHH, v_BNHK, "B N H M, B M H K -> B N H K"
-        )
+        else:
+            attn_scores_BNHH = einops.einsum(
+                q_BNHK, k_BNHK, "B N H K, B M H K -> B N H M"
+            ) / (self.K**0.5)
+            if causal:
+                mask_NM = torch.tril(torch.ones(N, N, device=x_BND.device)).bool()
+                attn_scores_BNHH = attn_scores_BNHH.masked_fill(
+                    ~mask_NM[None, :, None, :], float("-inf")
+                )
+            attn_probs_BNHH = torch.nn.functional.softmax(attn_scores_BNHH, dim=-1)
+            attn_output_BNHK = einops.einsum(
+                attn_probs_BNHH, v_BNHK, "B N H M, B M H K -> B N H K"
+            )
         attn_output_BND = attn_output_BNHK.reshape(B, N, D)
         output_BND = self.out(attn_output_BND)
         return output_BND

src/hyper_connections/train.py
@@ -4,23 +4,31 @@ from tqdm.auto import tqdm
 import wandb
 import huggingface_hub as hf
 
-from hyper_connections.model.hc import HubHCGPT
-from hyper_connections.util import get_device, get_num_params, get_tokenizer, get_tokenized_dolma_train_dataset, get_tokenized_c4_val_dataset
-from hyper_connections.eval import eval
+from transformers import OlmoForCausalLM
 
-# %%
-token = input("Enter your Hugging Face token: ")
-hf.login(token=token)
+from hyper_connections.model.gpt import GPTConfig
+from hyper_connections.model.hc import HubHCGPT
+from hyper_connections.util import get_device, get_num_params, get_tokenizer, get_tokenized_dolma_train_dataset, get_tokenized_c4_val_dataset, get_olmo
+from hyper_connections.eval import eval
 
 # %%
 device = get_device()
 device
 
+# # %%
+# model = get_olmo(device)
+# x = torch.randint(0, model.config.vocab_size, (1, 16), device=device)
+# model.forward(x, labels=x)
+
+# # %%
+# token = input("Enter your Hugging Face token: ")
+# hf.login(token=token)
+
 # %%
 # n_tokens = int(5e9)  # 5B tokens
 n_tokens = int(5e7)
 max_seq_len = 1024
-batch_size = 8
+batch_size = 1
 val_dataset = get_tokenized_c4_val_dataset(
     tokenizer=get_tokenizer(),
     n_tokens=1024 * 512,
@@ -29,8 +37,7 @@ val_dataset = get_tokenized_c4_val_dataset(
 )
 
 # %%
-# model = HubGPT(
-model = HubHCGPT(
+model_cfg = GPTConfig(
     # vocab_size=tokenizer.vocab_size,
     vocab_size=50304,  # 128 * 393
     dim=1024,
@@ -39,12 +46,14 @@ model = HubHCGPT(
     base=10_000,
     padding_idx=1,
 )
+# model = HubGPT(model_cfg)
+model = HubHCGPT(model_cfg)
 num_params_m = get_num_params(model) // 1_000_000
 print(f"{num_params_m}M parameters.")
 model.to(device)
 
 # %%
-use_wandb = True
+use_wandb = False
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, weight_decay=1e-2, betas=(0.9, 0.95))
 n_epoch = 1
 n_checkpoint_tokens = n_tokens // 10